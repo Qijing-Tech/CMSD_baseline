9378d1a74aa6a7f306df5628056e056d2a80355f
Zreal <zenglz_pro@163.com>
2021-01-06 21:47:22 +0800

    data root

--- config.py
+++ config.py
@@ -11,12 +11,21 @@ data_name : [sm_HIT_syn, ex_HIT_syn]
 word_emb_select : [combined.embed, Tencent_combined.embed]
 method_type : [kmeans, gmms, dbscan, ac]
 '''
+
+from pathlib import Path
+cwd = Path.cwd()
+
+# ------------- path config ------------- #
+DATA_ROOT = cwd.joinpath('data')
+
+# ------------- data config ------------- #
 DataConfig = {
-    'data_dir' : '../data',
+    # 'data_dir' : '../data',
     'data_name' : 'ex_HIT_syn',
-    'word_emb_select' :'combined.embed',
+    'word_emb_select' :'Tencent_combined.embed',
     'method_type' : 'ac',
     'run_times' : 5,
     'seed_list': [1,1234,1314,2020,2021],
     'log_file' : 'record.txt',
 }
+
